65bd9fc1e10c3580a6f0c81691180fe49785b263
Fathima Zohara <fathimaakifa35@gmail.com>
2024-06-23 23:51:33 +0530

    student management

--- backend/main/serializers.py
+++ backend/main/serializers.py
@@ -5,7 +5,7 @@ from .models import User, Student, Teacher, ClassRoom, Subject
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'password', 'first_name', 'last_name', 'date_of_birth', 'address', 'phone_number', 'profile_picture', 'is_student', 'is_teacher', 'is_admin']
+        fields = ['id', 'username', 'email', 'password', 'first_name', 'last_name', 'date_of_birth', 'address', 'phone_number', 'profile_picture', 'is_student', 'is_teacher', 'is_admin', 'is_active']
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
@@ -44,12 +44,6 @@ class StudentSerializer(serializers.ModelSerializer):
         student = Student.objects.create(user=user, **validated_data)
         return student
 
-class TeacherSerializer(serializers.ModelSerializer):
-    user = UserSerializer()
-
-    class Meta:
-        model = Teacher
-        fields = ['user', 'subject', 'joined_date']
 
     def create(self, validated_data):
         user_data = validated_data.pop('user')
@@ -63,3 +57,11 @@ class SubjectSerializer(serializers.ModelSerializer):
     class Meta:
         model = Subject
         fields = '__all__'
+
+        
+class TeacherSerializer(serializers.ModelSerializer):
+    user = UserSerializer()
+    subject = SubjectSerializer()
+    class Meta:
+        model = Teacher
+        fields = ['user', 'subject', 'joined_date', 'subject']
